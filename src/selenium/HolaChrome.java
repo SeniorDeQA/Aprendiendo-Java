6edfc50cc6038e46d4c606400e6940394a91d709
dortiz <dortiz@PC03.ideable.es>
2017-05-17 13:05:15 +0200

    Signed-off-by: dortiz <dortiz@PC03.ideable.es>

--- src/selenium/HolaChrome.java
+++ src/selenium/HolaChrome.java
@@ -8,14 +8,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class HolaChrome {
 	
 	public static void main(String[] args) {
-		WebDriver driver;
+		
+		//debe ser url formal
+		String baseURL = "http://www.google.com";
 		
 		//Se inicializa pasando por su driver
 		System.setProperty("webdriver.chrome.driver", "C://Users//dortiz//workspace//selenium_3.4.0//chromedriver.exe");
-		
+		WebDriver driver;
 		driver =new ChromeDriver();
-		//debe ser url formal
-		String baseURL = "http://www.google.com";
+		
 			
 		driver.get(baseURL);
 	}
