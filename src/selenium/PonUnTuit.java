c5ffb9c9d2f97a8b386d8dc01fb454dc7d43a334
dortiz <dortiz@PC03.ideable.es>
2017-05-23 17:38:07 +0200

    Signed-off-by: dortiz <dortiz@PC03.ideable.es>

--- src/selenium/PonUnTuit.java
+++ src/selenium/PonUnTuit.java
@@ -1,6 +1,10 @@
 package selenium;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 
 public class PonUnTuit {
@@ -13,18 +17,35 @@ public class PonUnTuit {
 		
 		CuatroEnUno.driver.manage().window().maximize();
 		
-		try {
-			CuatroEnUno.driver.findElement(By.xpath("//*[@id='doc']/div[1]/div/div[1]/div[2]/a[3]")).click();
-			CuatroEnUno.driver.findElement(By.xpath("//*[@id='login-dialog-dialog']/div[2]/div[2]/div[2]/form/div[1]/input")).sendKeys("seniordeqa");
-			CuatroEnUno.driver.findElement(By.xpath("//*[@id='login-dialog-dialog']/div[2]/div[2]/div[2]/form/div[2]/input")).sendKeys("moravia");
-			CuatroEnUno.driver.findElement(By.xpath("html/body/div[22]/div[2]/div[2]/div[2]/div[2]/form/input[1]")).click();
+		//DesiredCapabilities caps = DesiredCapabilities.firefox();
+		//caps.setCapability(InternetExplorerDriver.NATIVE_EVENTS, true);
+
+		CuatroEnUno.driver.findElement(By.xpath("//*[@id='doc']/div[1]/div/div[1]/div[2]/a[3]")).click();
+		CuatroEnUno.driver.findElement(By.xpath("//*[@id='login-dialog-dialog']/div[2]/div[2]/div[2]/form/div[1]/input")).sendKeys("unbotcualquier1");
+		CuatroEnUno.driver.findElement(By.xpath("//*[@id='login-dialog-dialog']/div[2]/div[2]/div[2]/form/div[2]/input")).sendKeys("blabla");
+		CuatroEnUno.driver.findElement(By.xpath("html/body/div[22]/div[2]/div[2]/div[2]/div[2]/form/input[1]")).click();
 			
-			
-			
-		} catch (Exception e){
-		  System.out.println(e.getStackTrace());	
-		} 
-			
+		CuatroEnUno.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		CuatroEnUno.driver.findElement(By.id("global-new-tweet-button")).click();
+		CuatroEnUno.driver.findElement(By.xpath("html/body/div[10]/div[2]/div[2]/div[4]/form/div[2]/div[2]/div[2]/div[2]/div[1]")).sendKeys("Esto es una prueba");
+		CuatroEnUno.driver.findElement(By.xpath("html/body/div[10]/div[2]/div[2]/div[4]/form/div[3]/div[2]/button")).click();
+		
+		//modalposttuiteo - se evita dejando un tuit
+		//CuatroEnUno.driver.findElement(By.xpath("html/body/div[3]/div/div/a")).click();
+		
+		//borrar tuit
+		CuatroEnUno.driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		CuatroEnUno.driver.findElement(By.cssSelector(".ProfileTweet-actionButton.u-textUserColorHover.dropdown-toggle.js-dropdown-toggle"));
+				//xpath("html/body/div[2]/div[2]/div/div[2]/div[4]/div[2]/ol[1]/li[1]/div[1]/div[2]/div[1]/div/div/button")).click();
+		CuatroEnUno.driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		CuatroEnUno.driver.findElement(By.xpath("html/body/div[2]/div[2]/div/div[2]/div[4]/div[2]/ol[1]/li[1]/div[1]/div[2]/div[1]/div/div/div/ul/li[7]/button")).click();
+		CuatroEnUno.driver.findElement(By.cssSelector("btn.primary-btn.delete-action")).click();
+		
+		//logout
+		CuatroEnUno.driver.findElement(By.xpath("html/body/div[2]/div[1]/div[2]/div/div/div[3]/ul/li[1]/a")).click();
+		CuatroEnUno.driver.findElement(By.xpath("html/body/div[2]/div[1]/div[2]/div/div/div[3]/ul/li[1]/div/ul/li[12]/button")).click();
+		
 			
 			
 			
